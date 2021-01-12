856baa8fc26328abff1daa201abbd724a34dfb8b
mabdallah-codes <abdallah.firmo@gmail.com>
2021-01-12 09:14:45 +0100

    Update

---
+++ decision_tree.py
@@ -0,0 +1,26 @@
+# Develop a classification model using Decision Tree Algorithm
+
+import numpy as np
+import pandas as pd 
+from sklearn.tree import DecisionTreeClassifier
+
+
+# Dataset
+
+"""
+Imagine that you are a medical researcher compiling data for a study. You have collected data about a set of patients, all of whom suffered from the same illness. During their course of treatment, each patient responded to one of 5 medications, Drug A, Drug B, Drug c, Drug x and y.
+
+Part of your job is to build a model to find out which drug might be appropriate for a future patient with the same illness. The feature sets of this dataset are Age, Sex, Blood Pressure, and Cholesterol of patients, and the target is the drug that each patient responded to.
+
+It is a sample of multiclass classifier, and you can use the training part of the dataset to build a decision tree, and then use it to predict the class of a unknown patient, or to prescribe it to a new patient. 
+
+"""
+
+# Downloading the Data
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/drug200.csv"
+
+df = pd.read_csv(url)
+
+df.head()
+df.describe()
